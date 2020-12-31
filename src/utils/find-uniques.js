28f669e6ca0da7a92eab16b4ef271bfe3321467d
anders <andersb@tuta.io>
2020-12-31 04:46:20 -0600

    cleaner findUniques

--- src/utils/find-uniques.js
+++ src/utils/find-uniques.js
@@ -1,12 +1,12 @@
 'use strict'
 
 function findUniques (value, key) {
-  // Create an index of the collection
-  const uniques = {}
-  const get = e => uniques[e]
-  const addToIndex = e => (uniques[key ? e[key] : e] = e)
-  value.forEach(addToIndex)
-  return Object.keys(uniques).map(get)
+  const uniques = new Set()
+  return value.filter(e => {
+    e = key ? e[key] : e
+    // if uniques has e return false, otherwise add e and return true
+    return !uniques.has(e) && uniques.add(e)
+  })
 }
 
 module.exports = findUniques
