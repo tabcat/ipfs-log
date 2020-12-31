30be224eef2191b2224b163e164750ba5f4d8077
anders <andersb@tuta.io>
2020-12-31 15:17:28 -0600

    simplify difference util fn

--- src/utils/difference.js
+++ src/utils/difference.js
@@ -1,26 +1,10 @@
 'use strict'
 
+const findUniques = require('./find-uniques')
+
 function difference (a, b, key) {
-  // Indices for quick lookups
-  const processed = {}
-  const existing = {}
-
-  // Create an index of the first collection
-  const addToIndex = e => (existing[key ? e[key] : e] = true)
-  a.forEach(addToIndex)
-
-  // Reduce to entries that are not in the first collection
-  const reducer = (res, entry) => {
-    const isInFirst = existing[key ? entry[key] : entry] !== undefined
-    const hasBeenProcessed = processed[key ? entry[key] : entry] !== undefined
-    if (!isInFirst && !hasBeenProcessed) {
-      res.push(entry)
-      processed[key ? entry[key] : entry] = true
-    }
-    return res
-  }
-
-  return b.reduce(reducer, [])
+  // get set difference of b and a
+  return findUniques(b, key, findUniques(a, key))
 }
 
 module.exports = difference
--- src/utils/find-uniques.js
+++ src/utils/find-uniques.js
@@ -1,9 +1,10 @@
 'use strict'
 
-function findUniques (value, key) {
-  const uniques = new Set()
+function findUniques (value, key, existing = []) {
+  const findE = e => key ? e[key] : e
+  const uniques = new Set(existing.map(findE))
   return value.filter(e => {
-    e = key ? e[key] : e
+    e = findE(e)
     // if uniques has e return false, otherwise add e and return true
     return !uniques.has(e) && uniques.add(e)
   })
